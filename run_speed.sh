5f6146be6eac4c168aa98251e33195a712b57af1
paulologeh <ologehmamezi@gmail.com>
2020-12-26 12:38:04 +0000

    replaced symlink with actual file@

--- run_speed.sh
+++ run_speed.sh
@@ -9,6 +9,7 @@ if ping -q -c 1 -W 1 google.co.uk >/dev/null; then
 else
 	echo "$day:$time Internet connection down" >> event.log
 	echo "$day,$time,0,0,none,none" >> speed_data.csv
+  cp speed_data.csv data-viewer
   exit 1 
 fi
 FILE=result.txt
@@ -19,3 +20,4 @@ host=`grep 'Hosted' $FILE | awk '{print $3,$4,$5}'`
 provider=`grep 'Testing from' $FILE | awk '{print $3}'`
 echo $day,$time,$download,$upload,$host,$provider >> speed_data.csv
 echo "$day:$time Speed test completed" >> event.log
+cp speed_data.csv ./data-viewer/public/speed_data.csv
