22ae79a511ad94c0246724f2576918dc743ec590
paulologeh <ologehmamezi@gmail.com>
2021-02-06 00:58:29 +0000

    updating to database

--- run_speed.sh
+++ run_speed.sh
@@ -4,12 +4,10 @@ time=$(date +"%T")
 day=$(date +"%D")
 echo "$day:$time Attempting speed test" >> event.log
 # check for internet connection
-if ping -q -c 1 -W 1 google.co.uk >/dev/null; then
+if ping -q -c 1 -W 1 google.com >/dev/null; then
   :
 else
 	echo "$day:$time Internet connection down" >> event.log
-	echo "$day,$time,0,0,none,none" >> speed_data.csv
-  cp speed_data.csv ./data-viewer/public/speed_data.csv
   exit 1 
 fi
 FILE=result.txt
@@ -19,5 +17,7 @@ upload=`grep 'Upload' $FILE | awk '{print $2}'`
 host=`grep 'Hosted' $FILE | awk '{print $3,$4,$5}'`
 provider=`grep 'Testing from' $FILE | awk '{print $3}'`
 echo $day,$time,$download,$upload,$host,$provider >> speed_data.csv
+echo "INSERT INTO testresults(RecordTime,Download,Upload,Host,Provider)VALUES ('$day $time',$download,$upload,'$host','$provider');" > update_database.sql
 echo "$day:$time Speed test completed" >> event.log
+sqlcmd -U admin -P $AWSPASSWD -S $AWSRDS -d speedtest -i update_database.sql
 cp speed_data.csv ./data-viewer/public/speed_data.csv
