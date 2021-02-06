06b8b5caaed8fd2f9fbe7b24530a805f3a6d7853
paulologeh <ologehmamezi@gmail.com>
2021-02-06 10:35:28 +0000

    improved internet checks

--- run_speed.sh
+++ run_speed.sh
@@ -3,21 +3,24 @@ source ~/.bashrc
 time=$(date +"%T")
 day=$(date +"%D")
 echo "$day:$time Attempting speed test" >> event.log
-# check for internet connection
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-  :
-else
-	echo "$day:$time Internet connection down" >> event.log
-  exit 1 
-fi
 FILE=result.txt
 speedtest-cli > $FILE
+
+if [ $? == 1 ]; then
+  echo "$day:$time Internet connection down" >> event.log
+  exit 1
+fi
+
 download=`grep 'Download' $FILE | awk '{print $2}'`
 upload=`grep 'Upload' $FILE | awk '{print $2}'`
 host=`grep 'Hosted' $FILE | awk '{print $3,$4,$5}'`
 provider=`grep 'Testing from' $FILE | awk '{print $3}'`
 echo $day,$time,$download,$upload,$host,$provider >> speed_data.csv
-echo "INSERT INTO testresults(RecordTime,Download,Upload,Host,Provider)VALUES ('$day $time',$download,$upload,'$host','$provider');" > update_database.sql
+QUERY="INSERT INTO testresults(RecordTime,Download,Upload,Host,Provider)VALUES ('$day $time',$download,$upload,'$host','$provider');"
 echo "$day:$time Speed test completed" >> event.log
-sqlcmd -U admin -P $AWSPASSWD -S $AWSRDS -d speedtest -i update_database.sql
-cp speed_data.csv ./data-viewer/public/speed_data.csv
+sqlcmd -U admin -P $AWSPASSWD -S $AWSRDS -d speedtest -Q "$QUERY"
+
+if [ $? == 1 ]; then
+  echo "ERROR!: Database upload failed" >> event.log
+  exit 1
+fi
