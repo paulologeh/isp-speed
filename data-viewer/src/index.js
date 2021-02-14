322e6a74eee8165dc8bedc117f777a2ab8507fba
paulologeh <ologehmamezi@gmail.com>
2021-02-14 15:49:06 +0000

    IE support

--- data-viewer/src/index.js
+++ data-viewer/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
